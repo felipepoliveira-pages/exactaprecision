7e209979b3fc0d2982c13337675b1aaca9ef6dff
Felipe Oliveira <felipeperoliveira@outlook.com>
2018-04-02 21:56:24 -0300

    Adding slick slider

---
+++ js/ep.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+    $('.slider').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+      });         
+});
